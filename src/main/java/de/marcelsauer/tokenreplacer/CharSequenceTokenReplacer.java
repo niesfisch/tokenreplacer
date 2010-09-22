9ff50ae154f6610c980516ce912df8fbe19c2f35
Marcel Sauer <marcel.sauer@gmx.de>
2010-09-22 20:51:30 +0200

    more logic

--- src/main/java/de/marcelsauer/tokenreplacer/CharSequenceTokenReplacer.java
+++ src/main/java/de/marcelsauer/tokenreplacer/CharSequenceTokenReplacer.java
@@ -1,3 +1,22 @@
+/**
+ * Token Replacer
+ * Copyright (C) 2010 Marcel Sauer <marcel DOT sauer AT gmx DOT de>
+ *
+ * This file is part of Token Replacer.
+ *
+ * Token Replacer is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Token Replacer is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Token Replacer. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.marcelsauer.tokenreplacer;
 
 import java.text.CharacterIterator;
@@ -8,21 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-/**
- * Token Replacer Copyright (C) 2010 Marcel Sauer <marcel DOT sauer AT gmx DOT de>
- * 
- * This file is part of Token Replacer.
- * 
- * Token Replacer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
- * version.
- * 
- * Token Replacer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with Token Replacer. If not, see
- * <http://www.gnu.org/licenses/>.
- */
+
 public class CharSequenceTokenReplacer implements TokenReplacer {
 
     /**
